8b6cc99d4d0a1a7137e058e6b830f2dd7ca507fc
Romain <romain.c@institutsolacroup.com>
2022-01-10 10:10:47 +0100

    Added port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs"); // File system
 const path = require("path");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const pathMonstersJSON = path.join(__dirname, "/data/monsters.json");
 const monsters = JSON.parse(fs.readFileSync(pathMonstersJSON).toString())
